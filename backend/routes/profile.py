1d12737a05949305e0ce159ef31df7a66673c178
oliiviiak <242837704+oliiviiak@users.noreply.github.com>
2026-02-28 23:31:28 -0800

    fixed auth and profile bugs

--- backend/routes/profile.py
+++ backend/routes/profile.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
-from features.create_functions import create_careneder, create_caretaker
+from features.create_functions import create_careneeder, create_caretaker
 import features.auth as auth
 
 router = APIRouter(prefix="/profile", tags=["profile"])
@@ -13,8 +13,8 @@ class CreateProfileRequest(BaseModel):
 
 @router.post("/")
 async def create_profile(req: CreateProfileRequest, user_id: str = Depends(auth.authenticate)):
-    if req.role == "careneder":
-        create_careneder(user_id, req.first_name, req.last_name, req.pfp)
+    if req.role == "careneeder":
+        create_careneeder(user_id, req.first_name, req.last_name, req.pfp)
     elif req.role == "caretaker":
         create_caretaker(user_id, req.first_name, req.last_name, req.pfp)
     else:
